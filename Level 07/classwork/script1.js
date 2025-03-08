feadadcdbfef3ec61300003cef474e8b662fc647
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-03-08 06:36:48 -0800

    commit

---
+++ Level 07/classwork/script1.js
@@ -0,0 +1,130 @@
+/* 1) alert - ის და console.log - ის გამოყენებით, გააკეთე 10-10 მაგალითი, ყველა მათემათიკურ ოპერატორზე(+,-,*,/,**) */ 
+
+/*
+console.log(10 + 10);
+console.log(5 + 5);
+console.log(3 + 3);
+console.log(20 + 10);
+console.log(50 + 13);
+console.log(25 + 25);
+console.log(13 + 12);
+console.log(20 + 10);
+console.log(10 + 20);
+console.log(30 + 35);
+*/
+
+/*
+console.log(5 - 10);
+console.log(12 - 5);
+console.log(13 - 3);
+console.log(12 - 4);
+console.log(30 - 15);
+console.log(15 - 1);
+console.log(17 - 4);
+console.log(18 - 10);
+console.log(18 - 7);
+console.log(17 - 3);
+*/
+
+/*
+console.log(5 * 3);
+console.log(5 * 4);
+console.log(5 * 5);
+console.log(5 * 6);
+console.log(5 * 7);
+console.log(5 * 8);
+console.log(5 * 9);
+console.log(12 * 3);
+console.log(12 * 7);
+console.log(20 * 5);
+*/
+
+/*
+console.log(5 / 3);
+console.log(5 / 5);
+console.log(20 / 12);
+console.log(20 / 3);
+console.log(20 / 5);
+console.log(25 / 12);
+console.log(25 / 7);
+console.log(45 / 15);
+console.log(55 / 35);
+console.log(100 / 50);
+*/
+/*
+console.log(5 ** 2)
+console.log(5 ** 3); 
+console.log(10 ** 3);
+console.log(12 ** 5);
+console.log(20 ** 15);
+console.log(25 ** 9);
+console.log(35 ** 15);
+console.log(45 ** 35);
+console.log(55 ** 15);
+console.log(55 ** 23);
+*/
+
+/*
+window.alert(10 + 10);
+window.alert(15 + 12);
+window.alert(17 + 3);
+window.alert(18 + 5);
+window.alert(25 + 3);
+window.alert(50 + 50);
+window.alert(65 + 15);
+window.alert(75 + 50);
+window.alert(100 + 30);
+window.alert(120 + 65);
+*/
+
+/*
+window.alert(3 - 1);
+window.alert(5 - 2);
+window.alert(7 - 3);
+window.alert(7 - 5);
+window.alert(8 - 3);
+window.alert(45 - 35);
+window.alert(55 - 32);
+window.alert(65 - 33);
+window.alert(67 - 8);
+window.alert(88 - 45);
+*/
+
+/*
+window.alert(3 * 2);
+window.alert(4 * 3);
+window.alert(4 * 4);
+window.alert(5 * 2);
+window.alert(12 * 4);
+window.alert(12 * 8);
+window.alert(25 * 15);
+window.alert(35 * 25);
+window.alert(45 * 39)
+window.alert(100 * 11)
+*/
+
+/*
+window.alert(3 / 2);
+window.alert(5 / 3);
+window.alert(7 / 5);
+window.alert(12 / 8);
+window.alert(12 / 3);
+window.alert(12 / 1);
+window.alert(13 / 8);
+window.alert(17 / 9);
+window.alert(35 / 27);
+window.alert(45 / 37);
+*/
+
+/*
+window.alert(5 ** 3);
+window.alert(7 ** 2);
+window.alert(8 ** 4);
+window.alert(10 ** 2);
+window.alert(15 ** 5);
+window.alert(25 ** 25);
+window.alert(35 ** 25);
+window.alert(55 ** 12);
+window.alert(100 ** 58);
+window.alert(150 ** 5)
+*/
---
+++ Level 07/classwork/script2.js
@@ -0,0 +1,5 @@
+// 2) alert - ის და console.log - ის გამოყენებით, ორი სიტყვა შეართე ერთმანეთს
+/*
+console.log("GOA" + "BEST");
+alert("Java" + "Script");
+*/
